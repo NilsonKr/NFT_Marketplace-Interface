d07d73f5477706035aa2b8aa6e5ce482c29508dc
Nilson Diaz Perez <nilsonkr@192.168.1.102>
2021-12-08 17:56:49 -0500

    :sparkles: Pages with ChakraUI and react-router v6 setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Home } from "./Pages/Home";
 
-export const App = () => <h2>Hello World , Im Back ! :3</h2>;
+export const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h2>Hello , Welcome to the Home page</h2>
+    </div>
+  );
+};
